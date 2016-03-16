ea225f8228f007e8941bd57fda8f9e0d8f22a0d1
3don <litvinov.ne@gmail.com>
2016-03-16 16:39:51 +0600

    TestingExample

--- app/src/main/java/com/example/jobquery/JobQueryList.java
+++ app/src/main/java/com/example/jobquery/JobQueryList.java
@@ -11,18 +11,141 @@ import java.util.ArrayList;
 
 public class JobQueryList extends AppCompatActivity {
 
-    private ArrayList<String> jobQueryList;
-
-
+    private final static String CONSUMER_KEY;
+    private final static String SECRET_KEY;
+    private final static String OAUTH_CALLBACK_SCHEME = "x-oauthflow";
+    private OAuthClient client;
+    SharedPreferences prefs;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        onNewIntent(getIntent());
         setContentView(R.layout.activity_job_query_list);
+        if (savedInstanceState == null) {
+            getFragmentManager().beginTransaction().add(R.id.container, new PlaceholderFragment()).commit();
+        }
+        Uri uri = getIntent().getData();
+        if (uri != null && uri.getScheme().equals(OAUTH_CALLBACK_SCHEME)) {
+            String verifier = uri.getQueryParameter(OAuth.OAUTH_VERIFIER);
 
+            new ODeskRetrieveAccessTokenTask().execute(verifier);
+        }
+         
+        prefs = PreferenceManager.getDefaultSharedPreferences(this);
+        String token = prefs.getString("token", null);
+        String tokenSecret = prefs.getString("secret", null);
+
+        if (token != null && tokenSecret != null && client != null) {
+            client.setTokenWithSecret(token, tokenSecret);
+        } else {
+            if (client == null) {
+                //NOTE: KEY/SECRET PAIR MUST BE STORED IN A SAFE PLACE
+                //THIS PART OF ASSIGNING OF CONSUMER KEY IS AN EXAMPLE
+                Properties props = new Properties();
+                props.setProperty("consumerKey", ODESK_CONSUMER_KEY);
+                props.setProperty("consumerSecret", ODESK_CONSUMER_SECRET);
+
+                Config config = new Config(props);
+
+                client = new OAuthClient(config);
+            }
+            // authorize
+            new ODeskAuthorizeTask().execute();
+        }
     }
+    
+    public static class PlaceholderFragment extends Fragment {
+
+        public PlaceholderFragment() {}
+
+        @Override
+        public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                Bundle savedInstanceState) {
+            View rootView = inflater.inflate(R.layout.fragment_my, container, false);
+            return rootView;
+        }
+    }
+    
+     class ODeskAuthorizeTask extends AsyncTask<Void, Void, String> {
+
+        private Exception exception;
+
+        @Override
+        protected String doInBackground(Void... params) {
+            String authzUrl = client.getAuthorizationUrl();
+            // if your api key type is 'mobile', possibly you want to use
+            // oauth_callback in your application, then use
+            //String authzUrl = client.getAuthorizationUrl("x-app://your_callback");
+
+            startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse(authzUrl)));
+
+            return authzUrl;
+        }
+
+        @Override
+        protected void onPostExecute(String result) {
+            super.onPostExecute(result);
+            if (result != null) {
+                Toast.makeText(MyActivity.this, result, Toast.LENGTH_LONG).show();
+            }
+        }
+    }
+
+    class ODeskRetrieveAccessTokenTask extends AsyncTask<String, Void, String> {
+        @Override
+        protected String doInBackground(String... params) {
+            String verifier = params[0];
+
+            HashMap<String, String> token = client.getAccessTokenSet(verifier);
+            client.setTokenWithSecret(token.get("token"), token.get("secret"));
+
+            // Save token/secret in preferences
+            prefs.edit().putString("token", token.get("token"))
+                        .putString("secret", token.get("secret"))
+                        .commit();
+
+            return "ok - token is: " + prefs.getString("token", null);
+        }
+
+        @Override
+        protected void onPostExecute(String result) {
+            super.onPostExecute(result);
+            if (result != null) {
+                Toast.makeText(MyActivity.this, result, Toast.LENGTH_LONG).show();
+            }
+        }
+    }
+
+    class GetMyUIdTask extends AsyncTask<Void, Void, String> {
+        @Override
+        protected String doInBackground(Void... params) {
+            JSONObject json = null;
+            String token = prefs.getString("token", null);
+            Users users = new Users(client);
+
+            String myId = "";
+            try {
+                json = users.getMyInfo();
+                JSONObject user = json.getJSONObject("user");
+                myId = user.getString("id");
+            }
+            catch (JSONException e) {
+                e.printStackTrace();
+            }
+
+            return "Your UID is " + myId;
+        }
+
+        @Override
+        protected void onPostExecute(String result) {
+            super.onPostExecute(result);
+            if (result != null) {
+                Toast.makeText(MyActivity.this, result, Toast.LENGTH_LONG).show();
+            }
+        }
+    }
+    
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -48,6 +171,6 @@ public class JobQueryList extends AppCompatActivity {
 
     @Override
     protected void onNewIntent(Intent intent) {
-
+        super.onNewIntent(intent);
     }
 }
