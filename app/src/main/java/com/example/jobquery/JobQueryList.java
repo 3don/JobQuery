c1164408f51530696ffb01ca563f565ad9be5e11
3don <litvinov.ne@gmail.com>
2016-03-29 21:37:36 +0600

    работа над getView adapter

--- app/src/main/java/com/example/jobquery/JobQueryList.java
+++ app/src/main/java/com/example/jobquery/JobQueryList.java
@@ -1,11 +1,15 @@
 package com.example.jobquery;
 
-import android.app.Activity;
 import android.app.AlertDialog;
+import android.app.Dialog;
+import android.app.ListActivity;
+import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.net.Uri;
 import android.os.AsyncTask;
+import android.os.Environment;
 import android.preference.PreferenceManager;
 import android.os.Bundle;
 import android.util.Log;
@@ -13,41 +17,59 @@ import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.view.ViewGroup;
+import android.view.ViewParent;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
 import android.widget.Toast;
+import android.view.View.OnClickListener;
 
 import com.Upwork.api.Config;
 import com.Upwork.api.OAuthClient;
 import com.Upwork.api.Routers.Organization.Users;
 import org.json.JSONException;
 import org.json.JSONObject;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.ObjectInputStream;
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Properties;
+import java.util.zip.Inflater;
+
 import oauth.signpost.OAuth;
 
-public class JobQueryList extends Activity {
+public class JobQueryList extends ListActivity {
 
     private final static String CONSUMER_KEY="";
     private final static String SECRET_KEY="";
     private final static String OAUTH_CALLBACK_SCHEME = "x-oauthflow";
     private OAuthClient client;
     SharedPreferences prefs;
+    static ArrayList<JobQuery> jobQueryList;
     private final static String TAG = "myLog";
+    private ListView jobQueryListView;
     String authUrl;
-    private Button addQueryButton;
-
-
+    private File jobQueryFile;
+    private JobQueryAdapter jobQueryAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         onNewIntent(getIntent());
+
+        jobQueryListView = getListView();
         setContentView(R.layout.activity_job_query_list);
-        if (savedInstanceState == null) {
-        //    getFragmentManager().beginTransaction().add(R.id.container, new PlaceholderFragment()).commit();
-        }
-        addQueryButton = (Button) findViewById(R.id.add_query_button);
-        addQueryButton.setOnClickListener(addQueryButtonListener);
+        Button addQueryButton = (Button) findViewById(R.id.addQueryButton);
+        addQueryButton.setOnClickListener(addNewQueryButtonListener);
+        jobQueryFile = new File(getExternalFilesDir(null).getAbsolutePath()+"/JobQueryData.ser");
+        new LoadJobQueryTask().execute((Object[]) null);
 
         prefs = PreferenceManager.getDefaultSharedPreferences(this);
         String token = prefs.getString("token", null);
@@ -70,35 +92,106 @@ public class JobQueryList extends Activity {
                 if (token!=null)
                 Log.e(TAG, token);
             }
-
-
             // authorize
-            new ODeskAuthorizeTask().execute();
+           // new ODeskAuthorizeTask().execute();
         }
     }
 
-    View.OnClickListener addQueryButtonListener = new View.OnClickListener() {
+    private class LoadJobQueryTask extends AsyncTask<Object, Object, Object>{
+        @Override
+        protected Object doInBackground(Object... params) {
+            if (jobQueryFile.exists()){
+                try{
+                    ObjectInputStream input = new ObjectInputStream(new FileInputStream(jobQueryFile));
+                    jobQueryList = (ArrayList<JobQuery>) input.readObject();
+                }
+                catch (final Exception e){
+                    runOnUiThread(new Runnable() {
+                        @Override
+                        public void run() {
+                            Toast.makeText(JobQueryList.this, "Не загружен список запросов",Toast.LENGTH_SHORT).show();
+                            Log.e(TAG, e.toString());
+                        }
+                    });
+                }
+            }
+            if (jobQueryList == null) jobQueryList = new ArrayList<JobQuery>();
+            return (Object) null;
+        }
+        @Override
+        protected void onPostExecute(Object o) {
+            super.onPostExecute(o);
+
+            jobQueryAdapter = new JobQueryAdapter(JobQueryList.this, jobQueryList);
+            jobQueryListView.setAdapter(jobQueryAdapter);
+
+        }
+    }
+
+    public OnClickListener addNewQueryButtonListener = new OnClickListener() {
         @Override
         public void onClick(View v) {
             openAddDialog();
         }
     };
 
-    private void openAddDialog(){
+    public void openAddDialog(){
+        LayoutInflater inflater = (LayoutInflater) getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+
+        View view = inflater.inflate(R.layout.new_job_query, null);
+
+        AlertDialog.Builder inputDialog = new AlertDialog.Builder(this);
+        inputDialog.setView(view);
+        inputDialog.setTitle(R.string.input_dialog_title);
+        final EditText jobQueryNameEditText = (EditText) view.findViewById(R.id.nameEditText);
+        final EditText jobQueryKeyEditText = (EditText) view.findViewById(R.id.keyWordEditText);
+        inputDialog.setPositiveButton(R.string.add_job_query_button, new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                String name = jobQueryNameEditText.getText().toString();
+                String key = jobQueryKeyEditText.getText().toString();
+                Log.e(TAG, name + " " + key);
+                saveQuery(name, key);
+            }
+        });
+        inputDialog.setNegativeButton(R.string.cancel_button, null);
+        inputDialog.show();
     }
 
-  /*  public static class PlaceholderFragment extends Fragment {
+    public void openEditDialog(ViewParent v){
+        LayoutInflater inflater = (LayoutInflater) getSystemService(Context.LAYOUT_INFLATER_SERVICE);
 
-        public PlaceholderFragment() {}
+        View view = inflater.inflate(R.layout.new_job_query, null);
 
-        @Override
-        public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                                 Bundle savedInstanceState) {
-            View rootView = inflater.inflate(R.layout.fragment_my, container, false);
-            return rootView;
+        AlertDialog.Builder inputDialog = new AlertDialog.Builder(this);
+        inputDialog.setView(view);
+        inputDialog.setTitle(R.string.input_dialog_title);
+        final EditText jobQueryNameEditText = (EditText) view.findViewById(R.id.nameEditText);
+        final EditText jobQueryKeyEditText = (EditText) view.findViewById(R.id.keyWordEditText);
+        inputDialog.setPositiveButton(R.string.add_job_query_button, new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                String name = jobQueryNameEditText.getText().toString();
+                String key = jobQueryKeyEditText.getText().toString();
+                Log.e(TAG, name + " " + key);
+                saveQuery(name, key);
+            }
+        });
+        inputDialog.setNeutralButton(R.string.delete_button, null);
+        inputDialog.setNegativeButton(R.string.cancel_button, null);
+        inputDialog.show();
+    }
+
+    public void saveQuery(String name, String key) {
+         if (name.length() != 0 && key.length() != 0) {
+            jobQueryList.add(new JobQuery(name, key));
+            jobQueryAdapter.notifyDataSetChanged();
+            Log.e(TAG, "adapter was notified");
         }
+
     }
-*/
+
+
     class ODeskAuthorizeTask extends AsyncTask<Void, Void, String> {
 
         private Exception exception;
@@ -147,6 +240,7 @@ public class JobQueryList extends Activity {
             super.onPostExecute(result);
             if (result != null) {
                 Toast.makeText(JobQueryList.this, result, Toast.LENGTH_LONG).show();
+                Log.e(TAG, result);
             }
         }
     }
@@ -217,4 +311,51 @@ public class JobQueryList extends Activity {
             new ODeskRetrieveAccessTokenTask().execute(verifier);
         }
     }
+
+    private static class ViewHolder {
+        TextView nameTextView;
+        TextView newQueryTextView;
+        Button editButton;
+    }
+
+    private class JobQueryAdapter extends ArrayAdapter<JobQuery>{
+        private ArrayList<JobQuery> items;
+        private LayoutInflater inflater;
+
+        public JobQueryAdapter(Context context, ArrayList<JobQuery> items){
+            super(context,-1,items);
+            this.items = items;
+            inflater = (LayoutInflater) getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+            Log.e(TAG, "Created adapter");
+        }
+
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+            Log.e(TAG, "start getView");
+            ViewHolder viewHolder;
+            if (convertView==null){
+                convertView = inflater.inflate(R.layout.job_query_item, null);
+                viewHolder = new ViewHolder();
+                viewHolder.nameTextView = (TextView) convertView.findViewById(R.id.jobQueryNameTextView);
+                viewHolder.newQueryTextView = (TextView) convertView.findViewById(R.id.newJobOffersTextView);
+                viewHolder.editButton = (Button) convertView.findViewById(R.id.editQueryButton);
+                convertView.setTag(viewHolder);
+            }
+            else
+                viewHolder = (ViewHolder) convertView.getTag();
+            JobQuery jobQuery = items.get(position);
+            viewHolder.nameTextView.setText(jobQuery.getJobQueryName());
+            viewHolder.editButton.setTag(jobQuery);
+            viewHolder.editButton.setOnClickListener(editButtonListener);
+
+            return convertView;
+        }
+    }
+
+    OnClickListener editButtonListener = new OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            openEditDialog(v.getParent());
+        }
+    };
 }
